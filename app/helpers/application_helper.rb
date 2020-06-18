aa4c85fca09fcf2d2bbd4a848aa024946ac2dd58
[HN-Intern-RB]Nguyen The Hiep <nguyen.the.hiep@sun-asterisk.com>
2020-06-18 08:29:24 +0700

    chapter3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  #khai bao phuong thuc helper display the title
+  def full_title page_title = ""
+    base_title = "Ruby on Rails Tutorial Sample App"
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|vi/ do
+    resources :microposts
+    resources :users
+  end
+
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/contact"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
