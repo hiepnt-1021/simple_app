af9fb1d2baa296eb6f2d9096220250552668824e
[HN-Intern-RB]Nguyen The Hiep <nguyen.the.hiep@sun-asterisk.com>
2020-06-23 14:51:06 +0700

    chapter_5b

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/newUer'
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
-    get  "/help", to: "static_pages#help", as: :help
 
+    get "/help", to: "static_pages#help"
+    get "/login", to: "static_pages#login"
+    get "/signup", to: "static_pages#newUser"
   end
 end
